99258b3f5e7212bc3d1f902c7ca8f9c77f130fd8
Jeff Teeter <jefteete@cisco.com>
2021-12-21 10:40:55 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "apikey" {
+  type        = string
+  description = "API Key"
+}
+variable "secretkey" {
+  type        = string
+  description = "Secret Key or file location"
+}
+variable "endpoint" {
+  type        = string
+  description = "API Endpoint URL"
+  default     = "https://www.intersight.com"
+}
